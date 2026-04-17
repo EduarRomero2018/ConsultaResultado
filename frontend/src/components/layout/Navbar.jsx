34912c6bb7eaec3d498a5ce517198e079dc31768
Eduar Romero <el.g21@hotmail.com>
2026-04-17 11:59:39 -0500

    Modificacion
    * Se agregó la imagen corporativa
    * Se modificó el color

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -7,12 +7,24 @@ function Navbar() {
     const isActive = (path) => location.pathname === path;
 
     return (
-        <header className="bg-institucion-primary text-white shadow-lg">
-            <div className="max-w-4xl mx-auto flex items-center justify-between px-4 py-3">
-                <h1 className="text-xl font-bold tracking-wide">
-                    Consulta de Resultados de Electro
+        <header className="bg-institucion-primary text-white shadow-xl">
+            <div className="max-w-6xl mx-auto relative flex items-center px-4 py-3">
+                <div className="flex items-center">
+                    <img
+                        src="/images/Caminos.png"
+                        alt="Logo Caminos"
+                        className="h-10 w-auto"
+                    />
+                    <span className="pl-[5px]  font-semibold md:text-base text-lg">
+                        Caminos IPS
+                    </span>
+                </div>
+
+                <h1 className="absolute left-1/2 -translate-x-1/2 text-lg md:text-xl font-bold tracking-wide text-center">
+                    Consulta de Resultados
                 </h1>
-                <nav className="flex gap-4">
+
+                <nav className="ml-auto flex items-center gap-4">
                     <Link
                         to="/search"
                         className={`px-3 py-1 rounded transition-colors ${isActive('/search')
